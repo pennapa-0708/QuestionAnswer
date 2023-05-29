5c9eeb6a294cce11424444d6383cfd1b61033806
pennapa-0708 <pennapa3280@gmail.com>
2023-05-29 14:32:40 +0700

    done project

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+});
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule("sass")
+      .test(/\.sass$/)
+      .use("sass-loader")
+      .loader("sass-loader")
+      .end();
+  },
+};
